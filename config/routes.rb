1b9ea757eb764c223af2b769f2f3731e9fa8f771
Louis Kirkham <chew@talk21.com>
2021-02-22 21:49:10 +0000

    Adds pages controller
    Sets root as pages#index and sets root as path for non existing routes.
    Tests response for index
    Creates namespace for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :runners, param: :slug
+      resources :races, param: :slug
+      resources :orders, only: [:create, :destroy]
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all 
 end
